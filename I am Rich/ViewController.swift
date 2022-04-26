6d6ebd12645c854ff87a099d744e4639b986bab1
Biswamohan-creator <56292855+Biswamohan-creator@users.noreply.github.com>
2022-04-26 20:54:24 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by CHINAM DWARIKANATH PATRA on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
